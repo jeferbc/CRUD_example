ad706050a5ae76894beb130aa4064f9424f32e55
jeferbc <jeffe.bernal@gmail.com>
2017-03-01 19:06:08 -0500

    show files

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,5 +1,11 @@
 class FilesController < ApplicationController
   def index
-    @files = Dir["files/*"]
+    #@files = Dir["files/*"]
+    @files = Dir.entries("#{Dir.pwd}/files/").select {|f| !File.directory? f}
+  end
+
+  def show
+    @file = params[:name]
+    @data = File.read("#{Dir.pwd}/files/#{@file}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'files', to: 'files#index'
+  get 'files/:name', to: 'files#show', as: 'file'
 end
